466a3699a8dbe212061e47ebd87e63b5cd95cf53
rudenko2000 <rudenko2000@gmail.com>
2022-04-09 16:44:54 +0200

    django 09.04.2022

--- alpha/alpha/urls.py
+++ alpha/alpha/urls.py
@@ -19,5 +19,8 @@ import library.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', library.views.home),
+    path('', library.views.home,name="home"),
+    path("books/<int:book_id>",library.views.book, name = "book"),
+    path("authors/<int:author_id>",library.views.author, name = "author"),
+
 ]
--- alpha/library/views.py
+++ alpha/library/views.py
@@ -1,15 +1,41 @@
+import datetime
+
 from django.shortcuts import render
 from django.http import HttpRequest
-import datetime
+
+from .models import Book, Author
+
+
 
 # Create your views here.
 
 def home(request):
+
     now=datetime.datetime.now()
-    godzina_otwarcia_strony=f"{now.hour}:{now.minute}:{now.second}"
-    context = {'title': f"Pierwsza strona,",
-               'title2': f" user agent: {request.headers['User-Agent']}",
-               'title3': f" godzina otwarcia strony {godzina_otwarcia_strony}",
-               'dump': request}
+    books=Book.objects.all()
+    godzina_otwarcia_strony=f"godzina: {now.hour}, minuta: {now.minute}, sekunda: {now.second}."
+    przegladarka=request.headers['User-Agent']
+    time=datetime.datetime.now().strftime("%H:%M:%S")
+
+
+
+    context = {'title': f"django ALX backend,",
+               'title1': f"Pierwsza strona",
+               'title2': f" Przeglądarka: {przegladarka}",
+               'title3': f" godzina otwarcia strony: {godzina_otwarcia_strony}, czyli: {time}",
+               "books":books,
+               }
 
     return render(request, 'library/index.html', context)
+
+def book(request,book_id):
+   book=Book.objects.get(id=book_id)
+   return render(request, "library/book.html",{"book":book})
+
+def author(request,author_id):
+
+   author=Author.objects.get(id=author_id)
+   listabooks = author.book_set.all()
+   return render(request, "library/author.html",{"author":author, "listabooks": listabooks})
+
+
